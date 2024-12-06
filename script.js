c93a3c3330047a4131e535555a656dafa3652757
dimtrip <dgaidajus@gmail.com>
2024-12-06 10:30:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+"use strict";
+let playSound1 = () => new Audio("kick.wav").play();
+let playSound2 = () => new Audio("clap.wav").play();
+let playSound3 = () => new Audio("cowbell.wav").play();
---
+++ script.ts
@@ -0,0 +1,6 @@
+
+let playSound1 = () => new Audio("kick.wav").play()
+
+let playSound2 = () => new Audio("clap.wav").play()
+
+let playSound3 = () => new Audio("cowbell.wav").play()
